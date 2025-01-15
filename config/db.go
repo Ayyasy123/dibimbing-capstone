37cae761ebd9a3d516364cac3c2a69e295ccc13c
Syamil Taqiyuddin <syamil_t_ayyasy_x@telkomsel.co.id>
2025-01-15 15:01:14 +0700

    update config db

--- config/db.go
+++ config/db.go
@@ -21,7 +21,7 @@ func ConnectDatabase() {
 	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
 
 	// Konfigurasi koneksi database with xampp
-	dsn := "root:@tcp(127.0.0.1:3306)/dibimbing_capstone?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(127.0.0.1:3306)/capstone?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
